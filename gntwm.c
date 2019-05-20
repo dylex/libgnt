3554e2abbabc22da65b8db5900abb7776bae8109
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2019-05-19 22:47:23 -0400

    Fix some timeout source removal and typing.
    This fixes some warnings I get about removing a source with id == 0.

--- gntwm.c
+++ gntwm.c
@@ -144,7 +144,7 @@ static int widestringwidth(wchar_t *wide);
 
 static void ensure_normal_mode(GntWMPrivate *priv);
 static gboolean write_already(gpointer data);
-static int write_timeout;
+static guint write_timeout;
 static time_t last_active_time;
 static gboolean idle_update;
 static GList *act = NULL; /* list of WS with unseen activitiy */
@@ -2289,7 +2289,6 @@ write_already(gpointer data)
 	}
 
 	g_free(filename);
-	g_source_remove(write_timeout);
 	write_timeout = 0;
 	return FALSE;
 }
